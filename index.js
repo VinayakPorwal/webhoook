e5e8d43dc6c7744b01b3f4e85024785394a80b42
Vinayak Porwal <vinayakporwal5522@gmail.com>
2024-07-07 19:48:52 +0530

    Bug fix

--- index.js
+++ index.js
@@ -1,81 +1,134 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
-require("dotenv").config();
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import express from "express";
+import axios from "axios";
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
-const token = process.env.WHATSAPP_TOKEN;
-const verifyToken = process.env.VERIFY_TOKEN;
+const { WEBHOOK_VERIFY_TOKEN, GRAPH_API_TOKEN, PORT, GOOGLE_API_KEY } =
+  process.env;
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Webhook is listening on port ${process.env.PORT}`);
+const handleMessage = async (userMessage) => {
+  // Check if user message starts with "hey"
+
+  // Prepare the request data
+  const requestData = {
+    contents: [
+      {
+        parts: [{ text: userMessage }],
+      },
+    ],
+  };
+
+  try {
+    // Make POST request to Google Cloud Natural Language API endpoint
+    const response = await axios.post(
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`,
+      requestData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Handle API response
+    return response.data.candidates[0].content.parts[0].text; // Return generated content or handle it as needed
+  } catch (error) {
+    console.error("Error calling API:", error.message);
+    return error.message; // Throw error for further handling or logging
+  }
+};
+
+app.post("/webhook", async (req, res) => {
+  // Log incoming messages
+  // console.log("Incoming webhook message:", JSON.stringify(req.body, null, 2));
+
+  // Check if the webhook request contains a message
+  const message = req.body.entry?.[0]?.changes[0]?.value?.messages?.[0];
+
+  // Check if the incoming message contains text
+  if (message?.type === "text") {
+    // Extract the business number to send the reply from it
+    const businessPhoneNumberId =
+      req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+    const from = message.from;
+    const messageText = message.text.body.toLowerCase();
+
+    // Determine the response based on the message content
+    let responseText;
+    if (messageText.startsWith("hey")) {
+      responseText = await handleMessage(messageText);
+      // "*Appointment Reminder* \n Hello Name,\n Your appointment has been scheduled on 24-11-2024,On Day shift. \n Thank you for using our service. \n Best regards, Petmatrix";
+    } else {
+      return;
+    }
+    console.log(responseText);
+
+    // Send the reply message
+    await axios({
+      method: "POST",
+      url: `https://graph.facebook.com/v18.0/${businessPhoneNumberId}/messages`,
+      headers: {
+        Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        to: from,
+        text: { body: responseText },
+        context: {
+          message_id: message.id, // Shows the message as a reply to the original user message
+        },
+      },
+    });
+
+    // Mark the incoming message as read
+    // await axios({
+    //   method: "POST",
+    //   url: `https://graph.facebook.com/v18.0/${businessPhoneNumberId}/messages`,
+    //   headers: {
+    //     Authorization: `Bearer ${GRAPH_API_TOKEN}`,
+    //   },
+    //   data: {
+    //     messaging_product: "whatsapp",
+    //     status: "read",
+    //     message_id: message.id,
+    //   },
+    // });
+  }
+
+  res.sendStatus(200);
 });
 
-// Verify the callback URL from the WhatsApp Cloud API dashboard
+// Accepts GET requests at the /webhook endpoint. You need this URL to setup webhook initially.
+// Info on verification request payload: https://developers.facebook.com/docs/graph-api/webhooks/getting-started#verification-requests
 app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
-  const challenge = req.query["hub.challenge"];
   const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
 
-  if (mode && token) {
-    if (mode === "subscribe" && token === verifyToken) {
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
-  }
-});
-
-// Handle incoming messages from the WhatsApp Cloud API
-app.post("/webhook", (req, res) => {
-  const body = req.body;
-
-  console.log(JSON.stringify(body, null, 2));
-
-  if (body.object) {
-    const entry = body.entry && body.entry[0];
-    const changes = entry && entry.changes && entry.changes[0];
-    const value = changes && changes.value;
-    const messages = value && value.messages && value.messages[0];
-
-    if (messages) {
-      const phoneNumberId = value.metadata.phone_number_id;
-      const from = messages.from;
-      const msgBody = messages.text.body;
-
-      console.log(`Phone number ID: ${phoneNumberId}`);
-      console.log(`From: ${from}`);
-      console.log(`Message body: ${msgBody}`);
-
-      axios({
-        method: "POST",
-        url: `https://graph.facebook.com/v13.0/${phoneNumberId}/messages?access_token=${token}`,
-        data: {
-          messaging_product: "whatsapp",
-          to: from,
-          text: { body: `Hi, I'm Prasath. You said: ${msgBody}` },
-        },
-        headers: { "Content-Type": "application/json" },
-      })
-        .then((response) => {
-          console.log("Message sent successfully");
-        })
-        .catch((error) => {
-          console.error("Error sending message:", error);
-        });
-
-      res.sendStatus(200);
-    } else {
-      res.sendStatus(404);
-    }
+  // Check the mode and token sent are correct
+  if (mode === "subscribe" && token === WEBHOOK_VERIFY_TOKEN) {
+    // Respond with 200 OK and challenge token from the request
+    res.status(200).send(challenge);
+    console.log("Webhook verified successfully!");
   } else {
-    res.sendStatus(404);
+    // Respond with '403 Forbidden' if verify tokens do not match
+    res.sendStatus(403);
   }
 });
 
-// Root endpoint for testing
 app.get("/", (req, res) => {
-  res.status(200).send("Hello, this is the webhook setup");
+  res.send(`<pre>Nothing to see here.
+Checkout README.md to start.</pre>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
 });
